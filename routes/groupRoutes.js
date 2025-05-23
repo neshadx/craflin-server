276c13522bdb2a070dafbec400fed4c61eb3c202
neshadx <neshadimagify@gmail.com>
2025-05-23 20:18:39 +0600

    Added some code

--- routes/groupRoutes.js
+++ routes/groupRoutes.js
@@ -1,3 +1,32 @@
+// const express = require("express");
+// const router = express.Router();
+
+// const {
+//   getAllGroups,
+//   getGroupById,
+//   createGroup,
+//   joinGroup,
+//   getMyGroups,
+//   updateGroup,
+//   deleteGroup
+// } = require("../controllers/groupController");
+
+// const verifyJWT = require("../middlewares/verifyJWT");
+
+// // Public routes
+// router.get("/", getAllGroups);
+// router.get("/:id", verifyJWT, getGroupById);
+
+// // Protected routes
+// router.post("/", verifyJWT, createGroup);
+// router.patch("/join/:id", verifyJWT, joinGroup);
+// router.get("/my-groups/list", verifyJWT, getMyGroups);
+// router.put("/:id", verifyJWT, updateGroup);
+// router.delete("/:id", verifyJWT, deleteGroup);
+
+// module.exports = router;
+
+
 const express = require("express");
 const router = express.Router();
 
@@ -17,11 +46,13 @@ const verifyJWT = require("../middlewares/verifyJWT");
 router.get("/", getAllGroups);
 router.get("/:id", verifyJWT, getGroupById);
 
-// Protected routes
-router.post("/", verifyJWT, createGroup);
+//  TEMP: Make Create Group public for testing
+router.post("/", createGroup);
+
 router.patch("/join/:id", verifyJWT, joinGroup);
 router.get("/my-groups/list", verifyJWT, getMyGroups);
 router.put("/:id", verifyJWT, updateGroup);
 router.delete("/:id", verifyJWT, deleteGroup);
 
 module.exports = router;
+
